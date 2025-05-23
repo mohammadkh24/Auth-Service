0aae6c8938c0a52fb06a13683e8def9526c8971a
Mohammad <mohammadgithub2024@gmail.com>
2025-05-23 18:23:35 +0330

    add features : doc for api

---
+++ app.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const docRoutes = require("./document/swagger.routes");
+
+const app = express();
+
+// Req.body
+app.use(express.urlencoded({ limit: "30mb", extended: true }));
+app.use(express.json({ limit: "30mb" }));
+
+// Routes
+app.use("/document" , docRoutes)
+
+// Not-Found Page
+app.use((req,res) => {
+    return res.status(404).json({
+        message : "404! Path not found"
+    })
+})
+
+module.exports = app

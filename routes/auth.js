8e5ee0810671198a716013e2c92eb8dddab18eb8
Mohammad <mohammadgithub2024@gmail.com>
2025-05-23 18:49:58 +0330

    add features : auth routes

--- routes/auth.js
+++ routes/auth.js
@@ -3,6 +3,8 @@ const controller = require("../controllers/auth")
 
 const router = express.Router();
 
-router.route("/send" , )
+router.route("/send").post(controller.send);
+router.route("/verify").post(controller.verify);
+router.route("/me").get(controller.getMe);
 
 module.exports = router
